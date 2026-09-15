510aa2d1470a94803be12015e4b94b3ad90507e5
mebaDwudu <tewldebirhanu@gmail.com>
2026-09-15 19:38:15 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+          customer: path.resolve(__dirname, 'customer.html'),
+          admin: path.resolve(__dirname, 'admin.html'),
+          cashier: path.resolve(__dirname, 'cashier.html'),
+        },
+      },
+    },
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+  };
+});
